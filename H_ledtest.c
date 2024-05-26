b0d502c296bed4d3b05a5a67a0e48452003bca4b
tony0258 <130204182+tony0258@users.noreply.github.com>
2024-05-26 16:51:32 +0900

    Update H_ledtest.c

--- H_ledtest.c
+++ H_ledtest.c
@@ -9,8 +9,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 #define LED_DRIVER_NAME "/dev/periled"
-int main(){ ledLibInit();
-sleep(3);
-ledOnOff(1,1); sleep(1); ledOnOff(1,0); sleep(1); ledOnOff(1,1); sleep(1); ledOnOff(1,0); sleep(1); ledOnOff(2,1); sleep(1); ledOnOff(2,0); sleep(1); ledOnOff(5,1); sleep(1); ledOnOff(5,0); sleep(1);
-ledLibExit(); return 0;
+int main(){ 
+          ledLibInit();
+           sleep(3);
+           ledOnOff(1,1); 
+           sleep(1); 
+           ledOnOff(1,0); 
+           sleep(1); 
+           ledOnOff(1,1); 
+           sleep(1); 
+           ledOnOff(1,0); 
+           sleep(1); 
+           ledOnOff(2,1); 
+           sleep(1); 
+           ledOnOff(2,0); 
+           sleep(1); 
+           ledOnOff(5,1); 
+           sleep(1); 
+           ledOnOff(5,0); 
+           sleep(1);
+           ledLibExit(); 
+           return 0;
 }
